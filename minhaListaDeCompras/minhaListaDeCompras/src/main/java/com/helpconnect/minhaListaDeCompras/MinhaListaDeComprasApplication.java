6cd16f5b50be0277a32ec6f69f63f288605866ce
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-04-01 21:17:27 -0300

    Criacao de projeto para compor uma API de um sistema para gerenciamento de lista de compras, com calculo de orcamento, listagem de produtos a serem comprados, quantidade de produtos e seu preco

---
+++ minhaListaDeCompras/minhaListaDeCompras/src/main/java/com/helpconnect/minhaListaDeCompras/MinhaListaDeComprasApplication.java
@@ -0,0 +1,13 @@
+package com.helpconnect.minhaListaDeCompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaListaDeComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaListaDeComprasApplication.class, args);
+	}
+
+}
---
+++ minhaListaDeCompras/minhaListaDeCompras/src/test/java/com/helpconnect/minhaListaDeCompras/MinhaListaDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpconnect.minhaListaDeCompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaListaDeComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
